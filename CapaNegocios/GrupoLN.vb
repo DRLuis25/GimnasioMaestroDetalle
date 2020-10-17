dd6481f70fcc97adba69ab8cd54fbb8e599d5a3f
lplasencia <fantasticoxd@gmail.com>
2020-10-16 23:02:09 -0500

    Creado clase GrupoLN

---
+++ CapaNegocios/GrupoLN.vb
@@ -0,0 +1,3 @@
+﻿Public Class GrupoLN
+
+End Class
--- Form1.vb
+++ Form1.vb
@@ -17,7 +17,7 @@ Public Class Form1
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         'TODO: esta línea de código carga datos en la tabla 'GimnasioDataSet.sp_ListarCursos' Puede moverla o quitarla según sea necesario.
-        Me.Sp_ListarCursosTableAdapter.Fill(Me.GimnasioDataSet.sp_ListarCursos)
+        'Me.Sp_ListarCursosTableAdapter.Fill(Me.GimnasioDataSet.sp_ListarCursos)'
 
     End Sub
 
